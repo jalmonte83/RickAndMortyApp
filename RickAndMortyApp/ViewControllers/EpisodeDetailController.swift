a5bf3631931ff4b7af6b8b162338f8ebec738406
Jeffrey Almonte <jalmonte83@gmail.com>
2019-01-08 10:22:03 -0500

    Final push for Demo

--- RickAndMortyApp/ViewControllers/EpisodeDetailController.swift
+++ RickAndMortyApp/ViewControllers/EpisodeDetailController.swift
@@ -9,22 +9,31 @@
 import UIKit
 
 class EpisodeDetailController: UIViewController {
-
+    var episodes: EpisodeInfo!
+    @IBOutlet weak var id: UILabel!
+    
+    @IBOutlet weak var airDate: UILabel!
+    
+    @IBOutlet weak var characters: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = episodes.name
+        id?.text = "\(episodes.id)"
+        airDate.text = episodes.airDate
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
